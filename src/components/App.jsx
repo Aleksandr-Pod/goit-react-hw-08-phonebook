4ee1b1eb53f133b6b110ac561f1b88d81635ca5d
Aleksandr-Pod <stdfire@gmail.com>
2022-05-25 21:06:46 +0300

    some corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { Phonebook } from "./Phonebook/Phonebook";
 import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./NavBar/navBar";
+import { Phonebook } from "./Phonebook/Phonebook";
 
 export const App = () => {
   const greetings = "<h2> Hello, my friend </h2>";
@@ -8,9 +8,10 @@ export const App = () => {
   return (
 
     <Routes>
-      <Route path="/" elements={<NavBar />}/>;
-      <Route index elements={greetings}/>;
-      <Route path="/Phonebook" elements={<Phonebook />}/>;
+      <Route path="/" elements={<NavBar />} >;
+        <Route index elements={greetings}/>;
+        <Route path="/Phonebook" elements={<Phonebook />} />;
+      </Route>
     </Routes>
 
   )
--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,9 +1,11 @@
-
+import { Outlet } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <>
-      <h1>Navigation Bar</h1><hr/>
+      <h1>Navigation Bar</h1>
+      <hr />
+      <Outlet/>
     </>
   )
 }
