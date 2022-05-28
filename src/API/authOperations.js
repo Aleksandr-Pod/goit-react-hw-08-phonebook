a6e07a8cf115aa2292bf5c5037556ea5705fd753
Aleksandr-Pod <stdfire@gmail.com>
2022-05-28 16:08:06 +0300

    It Works

--- src/API/authOperations.js
+++ src/API/authOperations.js
@@ -2,11 +2,15 @@
 
 import axios from 'axios';
 import { addUser, toggleLogin, changeLoading, changeError } from 'Redux/authSlice';
-const baseUrl = 'https://connections-api.herokuapp.com/users/';
 
+const baseUrl = 'https://connections-api.herokuapp.com/users/';
 const token = {
     set(token) { axios.defaults.headers.common.Authorization = `Bearer ${token}`;},
     unset() { axios.defaults.headers.common.Authorization = '';}
+}
+const emptyUser = {
+    user: { name: '', email: '' },
+    token: null
 }
     
 export const register = (user) => dispatch => {
@@ -19,7 +23,7 @@ export const register = (user) => dispatch => {
         })   
         .finally(dispatch(changeLoading(false)));
 }
-export const login = ({name, email, password}) => dispatch => {
+export const login = ({ email, password}) => dispatch => {
     axios.post(`${baseUrl}login`, { email, password })
         .then(resp => {
             dispatch(addUser(resp.data));
@@ -33,10 +37,10 @@ export const login = ({name, email, password}) => dispatch => {
 export const logout = () => dispatch => {
     axios.post(`${baseUrl}logout`)
         .then(resp => {
-            dispatch(addUser(resp.data));
-            token.unset();
+            console.log('logOut response ok:', resp)
             dispatch(toggleLogin(false));
-            dispatch(addUser({ user: { name: '', email: '', }, token: null }))
+            dispatch(addUser(emptyUser));
+            token.unset();
         })
         .catch(error => dispatch(changeError(error.message)))
         .finally(dispatch(changeLoading(false)));
--- src/components/NavBar/UserMenu/userMenu.js
+++ src/components/NavBar/UserMenu/userMenu.js
@@ -2,20 +2,22 @@ import { UserEmail } from "./userMenu.styled";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logout } from "API/authOperations";
+import { useEffect } from "react";
 
 export const UserMenu = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const user = useSelector(state => state.auth.user.email);
 
-    const onLogOut = () => {
-        dispatch(logout(false));
-        navigate('/');
-    }
+    useEffect(() => {
+        navigate('/phonebook');
+        return () => navigate('/login');
+    }, [navigate])
+    
     return (
         <>
             <UserEmail>{user}</UserEmail>
-            <button type="button" on onClick={onLogOut}>LogOut</button>
+            <button type="button" on onClick={() => dispatch(logout())}>LogOut</button>
         </>
     )
 }
